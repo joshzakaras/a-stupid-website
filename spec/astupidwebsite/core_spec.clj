fe051f1ca11e457c3d5c236b102a5ec54ea03b18
joshzakaras <joshzakaras@gmail.com>
2025-04-16 15:12:19 -0700

    initial commit

---
+++ spec/astupidwebsite/core_spec.clj
@@ -0,0 +1,7 @@
+(ns astupidwebsite.core-spec
+  (:require [speclj.core :refer :all]
+            [astupidwebsite.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/astupidwebsite/core.clj
@@ -0,0 +1,5 @@
+(ns astupidwebsite.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
